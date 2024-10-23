5fbe29895d1d40ff6a58bae9d6fd0be6ffc40c3a
Andy Roberto Jimenez Macnab <andy.jimenez@bam.com.gt>
2024-10-23 07:52:14 -0600

    Implementando JPA

---
+++ src/main/java/com/miumg/chatbottelegram/model/ChatMessage.java
@@ -0,0 +1,54 @@
+package com.miumg.chatbottelegram.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "chatMessages")
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String question;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String answer;
+
+    @Column(nullable = false)
+    private String client;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+}
